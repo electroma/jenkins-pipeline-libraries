076728669dde5e7ddcee08665a93ab8f46c5bc99
rsafronov <electroma@gmail.com>
2016-11-29 12:25:46 -0500

    fixed pipeline script

--- pipelines/meta-pipeline.groovy
+++ pipelines/meta-pipeline.groovy
@@ -2,76 +2,48 @@ try {
     currentBuild.result = "SUCCESS"
 
     stage('load libraries') {
-        node() {
-            shell = load "lib/shell.groovy"
-            pom = load "lib/pom.groovy"
-            git = load "lib/git.groovy"
-            jenkinsUnitRunner = load "src/it/jenkinsUnit/runner.groovy"
+        shellInst = new shell()
+        pomInst = new pom()
+        gitInst = new git()
 
-            repositoryUrl = shell.pipe("git config --get remote.origin.url")
-            pomVersion = pom.version(pwd() + "/pom.xml")
-        }
+        repositoryUrl = shellInst.pipe("git config --get remote.origin.url")
+        pomVersion = pomInst.version(pwd() + "/pom.xml")
     }
 
     stage('create package') {
-        node() {
-            def commitId = shell.pipe("git rev-parse HEAD")
+        def commitId = shellInst.pipe("git rev-parse HEAD")
 
-            sh("mvn clean package")
-            jenkinsUnitRunner.run("src/it/lib")
+        sh("mvn clean package")
 
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "git-credentials", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                def authenticatedUrl = git.authenticatedUrl(repositoryUrl, env.USERNAME, env.PASSWORD)
-                sh("git remote set-url origin ${authenticatedUrl} &> /dev/null")
-                sh("git tag -a ${pomVersion} -m \"Built version: ${pomVersion}\" ${commitId}")
-                sh("git push --tags")
-            }
-        }
-    }
-
-    stage('promote package') {
-        node() {
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "bintray-credentials", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                def credentials = "'${env.USERNAME}':'${env.PASSWORD}'"
-                sh("curl -u ${credentials} -T target/*.zip \"https://api.bintray.com/content/buildit/maven/jenkins-pipeline-libraries/${pomVersion}/jenkins-pipeline-libraries-${pomVersion}.zip?publish=1\"")
-            }
+        sshagent([credentialsId]) {
+            sh("git tag -a ${pomVersion} -m \"Built version: ${pomVersion}\" ${commitId}")
+            sh("git push --tags")
         }
     }
 
     stage('increment version') {
-        node() {
-            def majorVersion = pom.majorVersion(pwd() + "/pom.xml")
-            def minorVersion = pom.minorVersion(pwd() + "/pom.xml").toInteger()
-            def patchVersion = pom.patchVersion(pwd() + "/pom.xml").toInteger()
-            def newVersion = "${majorVersion}.${minorVersion + 1}.0"
-            if (patchVersion > 0) {
-                newVersion = "${majorVersion}.${minorVersion}.${patchVersion + 1}"
-            }
+        def majorVersion = pomInst.majorVersion(pwd() + "/pom.xml")
+        def minorVersion = pomInst.minorVersion(pwd() + "/pom.xml").toInteger()
+        def patchVersion = pomInst.patchVersion(pwd() + "/pom.xml").toInteger()
+        def newVersion = "${majorVersion}.${minorVersion + 1}.0"
+        if (patchVersion > 0) {
+            newVersion = "${majorVersion}.${minorVersion}.${patchVersion + 1}"
+        }
+        sh("mvn versions:set -DnewVersion=${newVersion} versions:commit")
 
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "git-credentials", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                sh("mvn versions:set -DnewVersion=${newVersion} versions:commit")
-                sh("git add pom.xml")
-                sh("git commit -m'Bumping version to ${newVersion}'")
-                sh("git push origin")
-            }
+        sshagent([credentialsId]) {
+            sh("git add pom.xml")
+            sh("git commit -m'Bumping version to ${newVersion}'")
+            sh("git push origin")
         }
     }
 }
 catch (err) {
     currentBuild.result = "FAILURE"
-    node() {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "git-credentials", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            // delete the tag off origin
-            sh("git push origin :refs/tags/${pomVersion}")
-            sh("git fetch --tags --prune")
-        }
+    sshagent([credentialsId]) {
+        // delete the tag off origin
+        sh("git push origin :refs/tags/${pomVersion}")
+        sh("git fetch --tags --prune")
     }
     throw err
-}
-finally {
-    stage('cleanup') {
-        node() {
-            sh("git remote set-url origin ${repositoryUrl} &> /dev/null")
-        }
-    }
 }
